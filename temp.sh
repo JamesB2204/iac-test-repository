584d15a173442203ea13add42e98771c97410a1a
JamesB2204 <jamescbradford22@gmail.com>
2026-03-05 12:28:53 +0000

    merging for testin

--- temp.sh
+++ temp.sh
@@ -6,5 +6,9 @@ modules_changed=$(terragrunt find --filter-affected --format json | jq .)
 
 for module in $(echo "$modules_changed" | jq -r '.[].path' | grep "$environment" ); do
     echo "Planning module: $module"
-  #terragrunt run --all plan --working-dir "$module"
+    terragrunt run --all --json-out-dir /tmp/json plan --working-dir "$module"
+    # terragrunt show -json "/tmp/plan_output_$module.tfplan" > "/tmp/plan_output_$module.json"
+    # rm "/tmp/plan_output_$module.tfplan"
+
 done
+
